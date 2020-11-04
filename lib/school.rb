859c9a2811071fd981e620471bf913e1e3a0b981
David Moina <davemolina777@gmail.com>
2020-11-04 15:20:52 -0600

    commit tree 911759600832898403795b3244fdcd95799be6a3

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
 # code here!
+require 'pry'
+class School
+    attr_accessor :roster 
+  
+    def initialize(name)
+        @name=name
+        @roster={}
+    end
+    def name
+        @name
+    end
+
+    # def grade
+    #     @grade
+    # end
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << name
+    end
+
+    def sort
+        # @roster[grade].collect do |name|
+        #     name.sort
+        # en  
+        @roster.collect do |grade, names|
+
+               names.sort! 
+        end
+
+        return @roster
+
+    end
+
+end
